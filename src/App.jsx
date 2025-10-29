25a36c9b6cfe1628f8dc421f7802f485c7f6f5ac
BhagatSingh-23 <ashukashyap15aug1947@gmail.com>
2025-10-29 08:38:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
           </span>
 
-          <div className='redP'>Valid from 29/08/2025 to 28/10/2025</div>
+          <div className='redP'>Valid from 14/09/2025 to 13/11/2025</div>
 
           <div className='foot-img'>
             <div className='sign-img'><img src="./sign.png" alt="" width={80} /></div>
